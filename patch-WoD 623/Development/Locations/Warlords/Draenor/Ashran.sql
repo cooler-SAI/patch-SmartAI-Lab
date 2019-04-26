881a28294c22ccfdd112e47757689ee57379fda5
cooler-SAI <cooler19860212@yahoo.com>
2019-04-26 20:36:47 +0300

    fixes

--- patch-WoD 623/Development/Locations/Warlords/Draenor/Ashran.sql
+++ patch-WoD 623/Development/Locations/Warlords/Draenor/Ashran.sql
@@ -420,7 +420,7 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,11,0,100,1,0,0,0,0,11,169668,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Fast Asleep on Spawn'),
-(@ENTRY,0,0,0,2,0,100,1,0,30,0,0,11,169710,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Cranky at 30% HP');
+(@ENTRY,0,1,0,2,0,100,1,0,30,0,0,11,169710,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Cranky at 30% HP');
 
 -- Goren Iron-eater
 SET @ENTRY := 78936;
