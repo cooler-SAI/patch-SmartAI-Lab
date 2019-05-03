f79eec14b43d5611b4c3342012f13d444f4e5155
cooler-SAI <cooler19860212@yahoo.com>
2019-05-03 13:40:26 +0300

    New files ( Stormheim)

--- patch-development/Development/Raids/Legion/The Nighthold.sql
+++ patch-development/Development/Raids/Legion/The Nighthold.sql
@@ -508,7 +508,7 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,0,0,3000,3500,11,225855,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Wrath'),
-(@ENTRY,0,1,0,9,0,100,0,0,6,15000,25000,11,225859,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Torrent of Water on Close');
+(@ENTRY,0,1,0,9,0,100,0,0,6,15000,25000,11,225859,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Torrent of Water on Close'),
 (@ENTRY,0,2,0,0,0,100,0,3000,5000,12000,15000,11,225858,0,0,0,0,0,4,0,0,0,0,0,0,0,'Cast Poison Brambles');
 
 -- Shambling HungererShambling Hungerer
