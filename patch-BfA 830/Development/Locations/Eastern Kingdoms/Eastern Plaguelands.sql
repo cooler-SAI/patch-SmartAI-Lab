d6488a9b7f0777ce98fca8d1a581dd491fea0bf3
cooler-SAI <cooler19860212@yahoo.com>
2020-08-18 19:06:17 +0300

    Ogres added missed

--- patch-BfA 830/Development/Locations/Eastern Kingdoms/Eastern Plaguelands.sql
+++ patch-BfA 830/Development/Locations/Eastern Kingdoms/Eastern Plaguelands.sql
@@ -382,6 +382,16 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,1,0,11,0,100,1,0,0,0,0,11,16577,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Disease Cloud on Spawn'),
 (@ENTRY,0,2,0,2,0,100,1,0,30,0,0,11,8599,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Enrage at 30% HP');
 
+-- Huntsman Leopold <The Scarlet Crusade>
+SET @ENTRY := 46094;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,0,0,2500,3000,11,6660,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Shoot'),
+(@ENTRY,0,1,0,0,0,100,0,3000,5000,12000,15000,11,79721,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Explosive Shot'),
+(@ENTRY,0,2,0,0,0,100,0,8000,10000,18000,22000,11,80003,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Black Arrow'),
+(@ENTRY,0,3,0,9,0,100,0,0,5,15000,25000,11,80004,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Snake Trap on Close');
+
 -- Infected Mossflayer
 SET @ENTRY := 12261;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
