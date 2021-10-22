bf6ec30b7e2aa8ee1b189643d1a3d15079ef5294
SPP User <spp-user@spp-forum.de>
2021-10-22 10:08:26 +0300

    Revert "DB/SAI: added Felfire Imp"
    This reverts commit 572d6a841e513eb590607b868d73e03e7df6e73b.

--- patch-BfA 837/!Milestone/Felwood.sql
+++ patch-BfA 837/!Milestone/Felwood.sql
@@ -280,13 +280,6 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,1,0,67,0,100,0,6000,11000,0,0,11,7159,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Backstab'),
 (@ENTRY,0,2,0,0,0,100,0,4000,7000,9000,12000,11,13579,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Gouge');
 
--- Jadefire Imp
-SET @ENTRY := 47369;
-UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
-DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,0,0,0,3000,3500,11,9053,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Fireball');
-
 -- Jadefire Satyr
 SET @ENTRY := 7105;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
